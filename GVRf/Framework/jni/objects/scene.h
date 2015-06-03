7179a90ce1ea1ce58f502be79814f7b6589a4be6
Tom Flynn <tom.flynn@samsung.com>
2015-06-02 17:13:38 -0700

    Ran formatter.
    Ran formatter on the portion of code that was editied.

--- GVRf/Framework/jni/objects/scene.h
+++ GVRf/Framework/jni/objects/scene.h
@@ -58,9 +58,19 @@ public:
     void set_occlusion_culling( bool occlusion_flag){ occlusion_flag_ = occlusion_flag; }
     bool get_occlusion_culling(){ return occlusion_flag_; }
 
-    void resetStats() { if(!statsInitialized) { Renderer::initializeStats(); statsInitialized = true; } Renderer::resetStats(); }
-    int getNumberDrawCalls() { return Renderer::getNumberDrawCalls(); }
-    int getNumberTriangles() { return Renderer::getNumberTriangles(); }
+    void resetStats() {
+        if (!statsInitialized) {
+            Renderer::initializeStats();
+            statsInitialized = true;
+        }
+        Renderer::resetStats();
+    }
+    int getNumberDrawCalls() {
+        return Renderer::getNumberDrawCalls();
+    }
+    int getNumberTriangles() {
+        return Renderer::getNumberTriangles();
+    }
 
 private:
     Scene(const Scene& scene);
