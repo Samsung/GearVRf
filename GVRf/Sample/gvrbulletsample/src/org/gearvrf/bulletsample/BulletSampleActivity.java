0b8a61ce81b0637f2b155e476e57b4684b109b09
Deepak Rawat <deepakrawat2287@gmail.com>
2015-06-12 18:27:14 -0700

    space in place of tab

--- GVRf/Sample/gvrbulletsample/src/org/gearvrf/bulletsample/BulletSampleActivity.java
+++ GVRf/Sample/gvrbulletsample/src/org/gearvrf/bulletsample/BulletSampleActivity.java
@@ -5,11 +5,11 @@ import org.gearvrf.GVRActivity;
 import android.os.Bundle;
 
 public class BulletSampleActivity extends GVRActivity {
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setScript(new BulletSampleViewManager(), "gvr_note4.xml");
-	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setScript(new BulletSampleViewManager(), "gvr_note4.xml");
+    }
 
 }
--- GVRf/Sample/gvrbulletsample/src/org/gearvrf/bulletsample/BulletSampleViewManager.java
+++ GVRf/Sample/gvrbulletsample/src/org/gearvrf/bulletsample/BulletSampleViewManager.java
@@ -28,192 +28,192 @@ import android.graphics.Color;
 
 public class BulletSampleViewManager extends GVRScript {
 
-	private GVRContext mGVRContext = null;
+    private GVRContext mGVRContext = null;
 
-	private Bullet mBullet = null;
+    private Bullet mBullet = null;
 
-	private Map<RigidBody, GVRSceneObject> rigidBodiesSceneMap = new HashMap<RigidBody, GVRSceneObject>();
+    private Map<RigidBody, GVRSceneObject> rigidBodiesSceneMap = new HashMap<RigidBody, GVRSceneObject>();
 
-	private static final float CUBE_MASS = 0.5f;
+    private static final float CUBE_MASS = 0.5f;
 
-	@Override
-	public void onInit(GVRContext gvrContext) throws Throwable {
-		mGVRContext = gvrContext;
+    @Override
+    public void onInit(GVRContext gvrContext) throws Throwable {
+        mGVRContext = gvrContext;
 
-		GVRScene scene = mGVRContext.getNextMainScene();
+        GVRScene scene = mGVRContext.getNextMainScene();
 
-		GVRCameraRig mainCameraRig = scene.getMainCameraRig();
-		mainCameraRig.getLeftCamera().setBackgroundColor(Color.BLACK);
-		mainCameraRig.getRightCamera().setBackgroundColor(Color.BLACK);
+        GVRCameraRig mainCameraRig = scene.getMainCameraRig();
+        mainCameraRig.getLeftCamera().setBackgroundColor(Color.BLACK);
+        mainCameraRig.getRightCamera().setBackgroundColor(Color.BLACK);
 
-		mainCameraRig.getOwnerObject().getTransform()
-				.setPosition(0.0f, 6.0f, 0.0f);
+        mainCameraRig.getOwnerObject().getTransform()
+                .setPosition(0.0f, 6.0f, 0.0f);
 
-		/*
-		 * Create new Bullet instance.
-		 * 
-		 * The Bullet Physics JNI used is from
-		 * http://www.badlogicgames.com/wordpress/?p=248 Its compiled as a jar
-		 * and native so file, sources can be found at
-		 * https://github.com/deepakrawat22/BulletJniFramework
-		 */
-		mBullet = new Bullet();
-		/*
-		 * Create the physics world.
-		 */
-		mBullet.createPhysicsWorld(new Vector3(-480.0f, -480.0f, -480.0f),
-				new Vector3(480.0f, 480.0f, 480.0f), 1024, new Vector3(0.0f,
-						-9.8f, 0.0f));
+        /*
+         * Create new Bullet instance.
+         * 
+         * The Bullet Physics JNI used is from
+         * http://www.badlogicgames.com/wordpress/?p=248 Its compiled as a jar
+         * and native so file, sources can be found at
+         * https://github.com/deepakrawat22/BulletJniFramework
+         */
+        mBullet = new Bullet();
+        /*
+         * Create the physics world.
+         */
+        mBullet.createPhysicsWorld(new Vector3(-480.0f, -480.0f, -480.0f),
+                new Vector3(480.0f, 480.0f, 480.0f), 1024, new Vector3(0.0f,
+                        -9.8f, 0.0f));
 
-		/*
-		 * Create the ground. A simple textured quad. In bullet it will be a
-		 * plane shape with 0 mass
-		 */
-		GVRSceneObject groundScene = quadWithTexture(100.0f, 100.0f,
-				"floor.jpg");
-		groundScene.getTransform().setRotationByAxis(-90.0f, 1.0f, 0.0f, 0.0f);
-		groundScene.getTransform().setPosition(0.0f, 0.0f, 0.0f);
-		scene.addSceneObject(groundScene);
+        /*
+         * Create the ground. A simple textured quad. In bullet it will be a
+         * plane shape with 0 mass
+         */
+        GVRSceneObject groundScene = quadWithTexture(100.0f, 100.0f,
+                "floor.jpg");
+        groundScene.getTransform().setRotationByAxis(-90.0f, 1.0f, 0.0f, 0.0f);
+        groundScene.getTransform().setPosition(0.0f, 0.0f, 0.0f);
+        scene.addSceneObject(groundScene);
 
-		StaticPlaneShape floorShape = new StaticPlaneShape(new Vector3(0.0f,
-				1.0f, 0.0f), 0.0f);
-		Geometry floorGeometry = mBullet.createGeometry(floorShape, 0.0f,
-				new Vector3(0.0f, 0.0f, 0.0f));
-		MotionState floorState = new MotionState();
-		mBullet.createAndAddRigidBody(floorGeometry, floorState);
+        StaticPlaneShape floorShape = new StaticPlaneShape(new Vector3(0.0f,
+                1.0f, 0.0f), 0.0f);
+        Geometry floorGeometry = mBullet.createGeometry(floorShape, 0.0f,
+                new Vector3(0.0f, 0.0f, 0.0f));
+        MotionState floorState = new MotionState();
+        mBullet.createAndAddRigidBody(floorGeometry, floorState);
 
-		/*
-		 * Create Some cubes in Bullet world and hit it with a sphere
-		 */
-		addCube(scene, 0.0f, 1.0f, -9.0f, CUBE_MASS);
-		addCube(scene, 0.0f, 1.0f, -10.0f, CUBE_MASS);
-		addCube(scene, 0.0f, 1.0f, -11.0f, CUBE_MASS);
-		addCube(scene, 1.0f, 1.0f, -9.0f, CUBE_MASS);
-		addCube(scene, 1.0f, 1.0f, -10.0f, CUBE_MASS);
-		addCube(scene, 1.0f, 1.0f, -11.0f, CUBE_MASS);
-		addCube(scene, 2.0f, 1.0f, -9.0f, CUBE_MASS);
-		addCube(scene, 2.0f, 1.0f, -10.0f, CUBE_MASS);
-		addCube(scene, 2.0f, 1.0f, -11.0f, CUBE_MASS);
+        /*
+         * Create Some cubes in Bullet world and hit it with a sphere
+         */
+        addCube(scene, 0.0f, 1.0f, -9.0f, CUBE_MASS);
+        addCube(scene, 0.0f, 1.0f, -10.0f, CUBE_MASS);
+        addCube(scene, 0.0f, 1.0f, -11.0f, CUBE_MASS);
+        addCube(scene, 1.0f, 1.0f, -9.0f, CUBE_MASS);
+        addCube(scene, 1.0f, 1.0f, -10.0f, CUBE_MASS);
+        addCube(scene, 1.0f, 1.0f, -11.0f, CUBE_MASS);
+        addCube(scene, 2.0f, 1.0f, -9.0f, CUBE_MASS);
+        addCube(scene, 2.0f, 1.0f, -10.0f, CUBE_MASS);
+        addCube(scene, 2.0f, 1.0f, -11.0f, CUBE_MASS);
 
-		addCube(scene, 0.0f, 2.0f, -9.0f, CUBE_MASS);
-		addCube(scene, 0.0f, 2.0f, -10.0f, CUBE_MASS);
-		addCube(scene, 0.0f, 2.0f, -11.0f, CUBE_MASS);
-		addCube(scene, 1.0f, 2.0f, -9.0f, CUBE_MASS);
-		addCube(scene, 1.0f, 2.0f, -10.0f, CUBE_MASS);
-		addCube(scene, 1.0f, 2.0f, -11.0f, CUBE_MASS);
-		addCube(scene, 2.0f, 2.0f, -9.0f, CUBE_MASS);
-		addCube(scene, 2.0f, 2.0f, -10.0f, CUBE_MASS);
-		addCube(scene, 2.0f, 2.0f, -11.0f, CUBE_MASS);
+        addCube(scene, 0.0f, 2.0f, -9.0f, CUBE_MASS);
+        addCube(scene, 0.0f, 2.0f, -10.0f, CUBE_MASS);
+        addCube(scene, 0.0f, 2.0f, -11.0f, CUBE_MASS);
+        addCube(scene, 1.0f, 2.0f, -9.0f, CUBE_MASS);
+        addCube(scene, 1.0f, 2.0f, -10.0f, CUBE_MASS);
+        addCube(scene, 1.0f, 2.0f, -11.0f, CUBE_MASS);
+        addCube(scene, 2.0f, 2.0f, -9.0f, CUBE_MASS);
+        addCube(scene, 2.0f, 2.0f, -10.0f, CUBE_MASS);
+        addCube(scene, 2.0f, 2.0f, -11.0f, CUBE_MASS);
 
-		addCube(scene, 0.0f, 3.0f, -9.0f, CUBE_MASS);
-		addCube(scene, 0.0f, 3.0f, -10.0f, CUBE_MASS);
-		addCube(scene, 0.0f, 3.0f, -11.0f, CUBE_MASS);
-		addCube(scene, 1.0f, 3.0f, -9.0f, CUBE_MASS);
-		addCube(scene, 1.0f, 3.0f, -10.0f, CUBE_MASS);
-		addCube(scene, 1.0f, 3.0f, -11.0f, CUBE_MASS);
-		addCube(scene, 2.0f, 3.0f, -9.0f, CUBE_MASS);
-		addCube(scene, 2.0f, 3.0f, -10.0f, CUBE_MASS);
-		addCube(scene, 2.0f, 3.0f, -11.0f, CUBE_MASS);
+        addCube(scene, 0.0f, 3.0f, -9.0f, CUBE_MASS);
+        addCube(scene, 0.0f, 3.0f, -10.0f, CUBE_MASS);
+        addCube(scene, 0.0f, 3.0f, -11.0f, CUBE_MASS);
+        addCube(scene, 1.0f, 3.0f, -9.0f, CUBE_MASS);
+        addCube(scene, 1.0f, 3.0f, -10.0f, CUBE_MASS);
+        addCube(scene, 1.0f, 3.0f, -11.0f, CUBE_MASS);
+        addCube(scene, 2.0f, 3.0f, -9.0f, CUBE_MASS);
+        addCube(scene, 2.0f, 3.0f, -10.0f, CUBE_MASS);
+        addCube(scene, 2.0f, 3.0f, -11.0f, CUBE_MASS);
 
-		/*
-		 * Throw a sphere from top
-		 */
-		addSphere(scene, 1.0f, 1.5f, 100.0f, -10.0f, 20.0f);
-	}
+        /*
+         * Throw a sphere from top
+         */
+        addSphere(scene, 1.0f, 1.5f, 100.0f, -10.0f, 20.0f);
+    }
 
-	@Override
-	public void onStep() {
-		mBullet.doSimulation(1.0f / 60.0f, 10);
-		for (RigidBody body : rigidBodiesSceneMap.keySet()) {
-			if (body.geometry.shape.getType() == ShapeType.SPHERE_SHAPE_PROXYTYPE) {
-				rigidBodiesSceneMap
-						.get(body)
-						.getTransform()
-						.setPosition(
-								body.motionState.resultSimulation.originPoint.x,
-								body.motionState.resultSimulation.originPoint.y,
-								body.motionState.resultSimulation.originPoint.z);
-			}
+    @Override
+    public void onStep() {
+        mBullet.doSimulation(1.0f / 60.0f, 10);
+        for (RigidBody body : rigidBodiesSceneMap.keySet()) {
+            if (body.geometry.shape.getType() == ShapeType.SPHERE_SHAPE_PROXYTYPE) {
+                rigidBodiesSceneMap
+                        .get(body)
+                        .getTransform()
+                        .setPosition(
+                                body.motionState.resultSimulation.originPoint.x,
+                                body.motionState.resultSimulation.originPoint.y,
+                                body.motionState.resultSimulation.originPoint.z);
+            }
 
-			if (body.geometry.shape.getType() == ShapeType.BOX_SHAPE_PROXYTYPE) {
-				rigidBodiesSceneMap
-						.get(body)
-						.getTransform()
-						.setPosition(
-								body.motionState.resultSimulation.originPoint.x,
-								body.motionState.resultSimulation.originPoint.y,
-								body.motionState.resultSimulation.originPoint.z);
-			}
-		}
-	}
+            if (body.geometry.shape.getType() == ShapeType.BOX_SHAPE_PROXYTYPE) {
+                rigidBodiesSceneMap
+                        .get(body)
+                        .getTransform()
+                        .setPosition(
+                                body.motionState.resultSimulation.originPoint.x,
+                                body.motionState.resultSimulation.originPoint.y,
+                                body.motionState.resultSimulation.originPoint.z);
+            }
+        }
+    }
 
-	private GVRSceneObject quadWithTexture(float width, float height,
-			String texture) {
-		FutureWrapper<GVRMesh> futureMesh = new FutureWrapper<GVRMesh>(
-				mGVRContext.createQuad(width, height));
-		GVRSceneObject object = null;
-		try {
-			object = new GVRSceneObject(mGVRContext, futureMesh,
-					mGVRContext.loadFutureTexture(new GVRAndroidResource(
-							mGVRContext, texture)));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return object;
-	}
+    private GVRSceneObject quadWithTexture(float width, float height,
+            String texture) {
+        FutureWrapper<GVRMesh> futureMesh = new FutureWrapper<GVRMesh>(
+                mGVRContext.createQuad(width, height));
+        GVRSceneObject object = null;
+        try {
+            object = new GVRSceneObject(mGVRContext, futureMesh,
+                    mGVRContext.loadFutureTexture(new GVRAndroidResource(
+                            mGVRContext, texture)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
 
-	private GVRSceneObject meshWithTexture(String mesh, String texture) {
-		GVRSceneObject object = null;
-		try {
-			object = new GVRSceneObject(mGVRContext, new GVRAndroidResource(
-					mGVRContext, mesh), new GVRAndroidResource(mGVRContext,
-					texture));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return object;
-	}
+    private GVRSceneObject meshWithTexture(String mesh, String texture) {
+        GVRSceneObject object = null;
+        try {
+            object = new GVRSceneObject(mGVRContext, new GVRAndroidResource(
+                    mGVRContext, mesh), new GVRAndroidResource(mGVRContext,
+                    texture));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
 
-	/*
-	 * Function to add a cube of unit size with mass at the specified position
-	 * in Bullet physics world and scene graph.
-	 */
-	private void addCube(GVRScene scene, float x, float y, float z, float mass) {
-		BoxShape boxShape = new BoxShape(new Vector3(0.5f, 0.5f, 0.5f));
-		Geometry boxGeometry = mBullet.createGeometry(boxShape, mass,
-				new Vector3(0.0f, 0.0f, 0.0f));
-		MotionState boxState = new MotionState();
-		boxState.worldTransform = new Transform(new Point3(x, y, z));
-		RigidBody boxBody = mBullet
-				.createAndAddRigidBody(boxGeometry, boxState);
+    /*
+     * Function to add a cube of unit size with mass at the specified position
+     * in Bullet physics world and scene graph.
+     */
+    private void addCube(GVRScene scene, float x, float y, float z, float mass) {
+        BoxShape boxShape = new BoxShape(new Vector3(0.5f, 0.5f, 0.5f));
+        Geometry boxGeometry = mBullet.createGeometry(boxShape, mass,
+                new Vector3(0.0f, 0.0f, 0.0f));
+        MotionState boxState = new MotionState();
+        boxState.worldTransform = new Transform(new Point3(x, y, z));
+        RigidBody boxBody = mBullet
+                .createAndAddRigidBody(boxGeometry, boxState);
 
-		GVRSceneObject cubeObject = meshWithTexture("cube.obj", "cube.jpg");
+        GVRSceneObject cubeObject = meshWithTexture("cube.obj", "cube.jpg");
 
-		cubeObject.getTransform().setPosition(x, y, z);
-		scene.addSceneObject(cubeObject);
-		rigidBodiesSceneMap.put(boxBody, cubeObject);
-	}
+        cubeObject.getTransform().setPosition(x, y, z);
+        scene.addSceneObject(cubeObject);
+        rigidBodiesSceneMap.put(boxBody, cubeObject);
+    }
 
-	/*
-	 * Function to add a sphere of dimension and position specified in the
-	 * Bullet physics world and scene graph
-	 */
-	private void addSphere(GVRScene scene, float radius, float x, float y,
-			float z, float mass) {
-		SphereShape sphereShape = new SphereShape(radius);
-		Geometry sphereGeometry = mBullet.createGeometry(sphereShape, mass,
-				new Vector3(0.0f, 0.0f, 0.0f));
-		MotionState sphereState = new MotionState();
-		sphereState.worldTransform = new Transform(new Point3(x, y, z));
-		RigidBody sphereBody = mBullet.createAndAddRigidBody(sphereGeometry,
-				sphereState);
+    /*
+     * Function to add a sphere of dimension and position specified in the
+     * Bullet physics world and scene graph
+     */
+    private void addSphere(GVRScene scene, float radius, float x, float y,
+            float z, float mass) {
+        SphereShape sphereShape = new SphereShape(radius);
+        Geometry sphereGeometry = mBullet.createGeometry(sphereShape, mass,
+                new Vector3(0.0f, 0.0f, 0.0f));
+        MotionState sphereState = new MotionState();
+        sphereState.worldTransform = new Transform(new Point3(x, y, z));
+        RigidBody sphereBody = mBullet.createAndAddRigidBody(sphereGeometry,
+                sphereState);
 
-		GVRSceneObject sphereObject = meshWithTexture("sphere.obj",
-				"sphere.jpg");
+        GVRSceneObject sphereObject = meshWithTexture("sphere.obj",
+                "sphere.jpg");
 
-		sphereObject.getTransform().setPosition(x, y, z);
-		scene.addSceneObject(sphereObject);
-		rigidBodiesSceneMap.put(sphereBody, sphereObject);
-	}
+        sphereObject.getTransform().setPosition(x, y, z);
+        scene.addSceneObject(sphereObject);
+        rigidBodiesSceneMap.put(sphereBody, sphereObject);
+    }
 
 }
