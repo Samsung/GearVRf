b28624f0986f1a5ded8251bf18b8d92bdff96703
Lokesh Basu <lokesh.basu@samsung.com>
2015-06-19 08:41:38 +0530

    Adds rotation to the model.

--- GVRf/Sample/gvrjassimpmodelloader/src/org/gearvrf/jassimpmodelloader/JassimpModelLoaderViewManager.java
+++ GVRf/Sample/gvrjassimpmodelloader/src/org/gearvrf/jassimpmodelloader/JassimpModelLoaderViewManager.java
@@ -16,12 +16,19 @@
 package org.gearvrf.jassimpmodelloader;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.gearvrf.GVRCameraRig;
 import org.gearvrf.GVRContext;
 import org.gearvrf.GVRScene;
 import org.gearvrf.GVRSceneObject;
 import org.gearvrf.GVRScript;
+import org.gearvrf.GVRTransform;
+import org.gearvrf.animation.GVRAnimation;
+import org.gearvrf.animation.GVRAnimationEngine;
+import org.gearvrf.animation.GVRRepeatMode;
+import org.gearvrf.animation.GVRRotationByAxisWithPivotAnimation;
 import org.gearvrf.utility.Log;
 
 import android.graphics.Color;
@@ -32,25 +39,73 @@ public class JassimpModelLoaderViewManager extends GVRScript {
     private static final String TAG = Log
             .tag(JassimpModelLoaderViewManager.class);
 
+    private GVRAnimationEngine mAnimationEngine;
+    private GVRScene mMainScene;
+
     @Override
     public void onInit(GVRContext gvrContext) throws IOException {
 
+        mAnimationEngine = gvrContext.getAnimationEngine();
+
+        mMainScene = gvrContext.getNextMainScene(new Runnable() {
+
+            @Override
+            public void run() {
+                for (GVRAnimation animation : mAnimations) {
+                    animation.start(mAnimationEngine);
+                }
+                mAnimations = null;
+            }
+        });
+
         GVRSceneObject model = gvrContext.getAssimpModel("astro_boy.dae");
 
-        model.getTransform().setRotationByAxis(-90.0f, 0.0f, 0.0f, 1.0f);
+        model.getTransform().setPosition(0.0f, -5.0f, 0.0f);
+        model.getTransform().setRotationByAxis(-180.0f, 0.0f, 0.0f, 1.0f);
 
-        GVRScene scene = gvrContext.getNextMainScene();
-
-        GVRCameraRig mainCameraRig = scene.getMainCameraRig();
+        GVRCameraRig mainCameraRig = mMainScene.getMainCameraRig();
         mainCameraRig.getLeftCamera().setBackgroundColor(Color.WHITE);
         mainCameraRig.getRightCamera().setBackgroundColor(Color.WHITE);
         mainCameraRig.getOwnerObject().getTransform()
                 .setPosition(0.0f, 0.0f, 0.0f);
 
-        scene.addSceneObject(model);
+        GVRSceneObject cameraRevolutionObject = new GVRSceneObject(gvrContext);
+        cameraRevolutionObject.getTransform().setPosition(4.0f, 0.0f, 0.0f);
+        cameraRevolutionObject.addChildObject(mMainScene.getMainCameraRig()
+                .getOwnerObject());
+
+        mMainScene.addSceneObject(model);
+
+        counterClockwise(cameraRevolutionObject, 60f);
+
+        clockwise(
+                mMainScene.getMainCameraRig().getOwnerObject().getTransform(),
+                120f);
     }
 
     @Override
     public void onStep() {
     }
+
+    private List<GVRAnimation> mAnimations = new ArrayList<GVRAnimation>();
+
+    private void setup(GVRAnimation animation) {
+        animation.setRepeatMode(GVRRepeatMode.REPEATED).setRepeatCount(-1);
+        mAnimations.add(animation);
+    }
+
+    private void counterClockwise(GVRSceneObject object, float duration) {
+        setup(new GVRRotationByAxisWithPivotAnimation( //
+                object, duration, 360.0f, //
+                0.0f, 1.0f, 0.0f, //
+                0.0f, 0.0f, 0.0f));
+    }
+
+    private void clockwise(GVRTransform transform, float duration) {
+        setup(new GVRRotationByAxisWithPivotAnimation( //
+                transform, duration, -360.0f, //
+                0.0f, 1.0f, 0.0f, //
+                0.0f, 0.0f, 0.0f));
+    }
+
 }
