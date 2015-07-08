661220560e2b02b473569ee048ae6e68e79a0ccb
Guodong Rong <g.rong@samsung.com>
2015-07-08 10:23:12 -0700

    Changed constant variables' names.

--- GVRf/Framework/src/org/gearvrf/scene_objects/GVRCubeSceneObject.java
+++ GVRf/Framework/src/org/gearvrf/scene_objects/GVRCubeSceneObject.java
@@ -56,7 +56,7 @@ public class GVRCubeSceneObject extends GVRSceneObject {
     //   13---------        ----------        22-------23
     //@formatter:on
 
-    private float[] simple_vertices = { -SIZE, -SIZE, SIZE, // 0
+    private static final float[] SIMPLE_VERTICES = { -SIZE, -SIZE, SIZE, // 0
             SIZE, -SIZE, SIZE, // 1
             -SIZE, SIZE, SIZE, // 2
             SIZE, SIZE, SIZE, // 3
@@ -87,7 +87,7 @@ public class GVRCubeSceneObject extends GVRSceneObject {
             SIZE, -SIZE, SIZE, // 23
     };
 
-    private float[] simple_outward_normals = { 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+    private static final float[] SIMPLE_OUTWARD_NORMALS = { 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
             1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
 
             1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
@@ -105,7 +105,7 @@ public class GVRCubeSceneObject extends GVRSceneObject {
             0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
             -1.0f, 0.0f };
 
-    private float[] simple_inward_normals = { 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+    private static final float[] SIMPLE_INWARD_NORMALS = { 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
             -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f,
 
             -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f,
@@ -123,7 +123,7 @@ public class GVRCubeSceneObject extends GVRSceneObject {
             0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
             0.0f };
 
-    private float[] simple_texCoords = { 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+    private static final float[] SIMPLE_TEXCOORDS = { 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
             1.0f, 0.0f, // front
             0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // right
             0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // back
@@ -132,7 +132,7 @@ public class GVRCubeSceneObject extends GVRSceneObject {
             0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom
     };
 
-    private char[] simple_outward_indices = { 0, 1, 2, // front
+    private static final char[] SIMPLE_OUTWARD_INDICES = { 0, 1, 2, // front
             2, 1, 3,
 
             4, 5, 6, // right
@@ -151,7 +151,7 @@ public class GVRCubeSceneObject extends GVRSceneObject {
             22, 21, 23 //
     };
 
-    private char[] simple_inward_indices = { 0, 2, 1, // front
+    private static final char[] SIMPLE_INWARD_INDICES = { 0, 2, 1, // front
             1, 2, 3,
 
             4, 6, 5, // right
@@ -288,15 +288,15 @@ public class GVRCubeSceneObject extends GVRSceneObject {
         GVRMesh mesh = new GVRMesh(gvrContext);
 
         if (facingOut) {
-            mesh.setVertices(simple_vertices);
-            mesh.setNormals(simple_outward_normals);
-            mesh.setTexCoords(simple_texCoords);
-            mesh.setTriangles(simple_outward_indices);
+            mesh.setVertices(SIMPLE_VERTICES);
+            mesh.setNormals(SIMPLE_OUTWARD_NORMALS);
+            mesh.setTexCoords(SIMPLE_TEXCOORDS);
+            mesh.setTriangles(SIMPLE_OUTWARD_INDICES);
         } else {
-            mesh.setVertices(simple_vertices);
-            mesh.setNormals(simple_inward_normals);
-            mesh.setTexCoords(simple_texCoords);
-            mesh.setTriangles(simple_inward_indices);
+            mesh.setVertices(SIMPLE_VERTICES);
+            mesh.setNormals(SIMPLE_INWARD_NORMALS);
+            mesh.setTexCoords(SIMPLE_TEXCOORDS);
+            mesh.setTriangles(SIMPLE_INWARD_INDICES);
         }
 
         GVRRenderData renderData = new GVRRenderData(gvrContext);
@@ -305,30 +305,30 @@ public class GVRCubeSceneObject extends GVRSceneObject {
         renderData.setMesh(mesh);
     }
 
-    private char[] simple_outward_front_indices = { 0, 1, 2, // front
+    private static final char[] SIMPLE_OUTWARD_FRONT_INDICES = { 0, 1, 2, // front
             2, 1, 3 };
-    private char[] simple_outward_right_indices = { 4, 5, 6, // right
+    private static final char[] SIMPLE_OUTWARD_RIGHT_INDICES = { 4, 5, 6, // right
             6, 5, 7 };
-    private char[] simple_outward_back_indices = { 8, 9, 10, // back
+    private static final char[] SIMPLE_OUTWARD_BACK_INDICES = { 8, 9, 10, // back
             10, 9, 11 };
-    private char[] simple_outward_left_indices = { 12, 13, 14, // left
+    private static final char[] SIMPLE_OUTWARD_LEFT_INDICES = { 12, 13, 14, // left
             14, 13, 15 };
-    private char[] simple_outward_top_indices = { 16, 17, 18, // top
+    private static final char[] SIMPLE_OUTWARD_TOP_INDICES = { 16, 17, 18, // top
             18, 17, 19 };
-    private char[] simple_outward_bottom_indices = { 20, 21, 22, // bottom
+    private static final char[] SIMPLE_OUTWARD_BOTTOM_INDICES = { 20, 21, 22, // bottom
             22, 21, 23 };
 
-    private char[] simple_inward_front_indices = { 0, 2, 1, // front
+    private static final char[] SIMPLE_INWARD_FRONT_INDICES = { 0, 2, 1, // front
             1, 2, 3 };
-    private char[] simple_inward_right_indices = { 4, 6, 5, // right
+    private static final char[] SIMPLE_INWARD_RIGHT_INDICES = { 4, 6, 5, // right
             5, 6, 7 };
-    private char[] simple_inward_back_indices = { 8, 10, 9, // back
+    private static final char[] SIMPLE_INWARD_BACK_INDICES = { 8, 10, 9, // back
             9, 10, 11 };
-    private char[] simple_inward_left_indices = { 12, 14, 13, // left
+    private static final char[] SIMPLE_INWARD_LEFT_INDICES = { 12, 14, 13, // left
             13, 14, 15 };
-    private char[] simple_inward_top_indices = { 16, 18, 17, // top
+    private static final char[] SIMPLE_INWARD_TOP_INDICES = { 16, 18, 17, // top
             17, 18, 19 };
-    private char[] simple_inward_bottom_indices = { 20, 22, 21, // bottom
+    private static final char[] SIMPLE_INWARD_BOTTOM_INDICES = { 20, 22, 21, // bottom
             21, 22, 23 };
 
     private void createSimpleCubeSixMeshes(GVRContext gvrContext,
@@ -341,32 +341,32 @@ public class GVRCubeSceneObject extends GVRSceneObject {
         }
 
         if (facingOut) {
-            meshes[0].setTriangles(simple_outward_front_indices);
-            meshes[1].setTriangles(simple_outward_right_indices);
-            meshes[2].setTriangles(simple_outward_back_indices);
-            meshes[3].setTriangles(simple_outward_left_indices);
-            meshes[4].setTriangles(simple_outward_top_indices);
-            meshes[5].setTriangles(simple_outward_bottom_indices);
+            meshes[0].setTriangles(SIMPLE_OUTWARD_FRONT_INDICES);
+            meshes[1].setTriangles(SIMPLE_OUTWARD_RIGHT_INDICES);
+            meshes[2].setTriangles(SIMPLE_OUTWARD_BACK_INDICES);
+            meshes[3].setTriangles(SIMPLE_OUTWARD_LEFT_INDICES);
+            meshes[4].setTriangles(SIMPLE_OUTWARD_TOP_INDICES);
+            meshes[5].setTriangles(SIMPLE_OUTWARD_BOTTOM_INDICES);
             for (int i = 0; i < 6; i++) {
-                meshes[i].setVertices(simple_vertices);
-                meshes[i].setNormals(simple_outward_normals);
-                meshes[i].setTexCoords(simple_texCoords);
+                meshes[i].setVertices(SIMPLE_VERTICES);
+                meshes[i].setNormals(SIMPLE_OUTWARD_NORMALS);
+                meshes[i].setTexCoords(SIMPLE_TEXCOORDS);
                 children[i] = new GVRSceneObject(gvrContext,
                         new FutureWrapper<GVRMesh>(meshes[i]),
                         futureTextureList.get(i));
                 addChildObject(children[i]);
             }
         } else {
-            meshes[0].setTriangles(simple_inward_front_indices);
-            meshes[1].setTriangles(simple_inward_right_indices);
-            meshes[2].setTriangles(simple_inward_back_indices);
-            meshes[3].setTriangles(simple_inward_left_indices);
-            meshes[4].setTriangles(simple_inward_top_indices);
-            meshes[5].setTriangles(simple_inward_bottom_indices);
+            meshes[0].setTriangles(SIMPLE_INWARD_FRONT_INDICES);
+            meshes[1].setTriangles(SIMPLE_INWARD_RIGHT_INDICES);
+            meshes[2].setTriangles(SIMPLE_INWARD_BACK_INDICES);
+            meshes[3].setTriangles(SIMPLE_INWARD_LEFT_INDICES);
+            meshes[4].setTriangles(SIMPLE_INWARD_TOP_INDICES);
+            meshes[5].setTriangles(SIMPLE_INWARD_BOTTOM_INDICES);
             for (int i = 0; i < 6; i++) {
-                meshes[i].setVertices(simple_vertices);
-                meshes[i].setNormals(simple_inward_normals);
-                meshes[i].setTexCoords(simple_texCoords);
+                meshes[i].setVertices(SIMPLE_VERTICES);
+                meshes[i].setNormals(SIMPLE_INWARD_NORMALS);
+                meshes[i].setTexCoords(SIMPLE_TEXCOORDS);
                 children[i] = new GVRSceneObject(gvrContext,
                         new FutureWrapper<GVRMesh>(meshes[i]),
                         futureTextureList.get(i));
