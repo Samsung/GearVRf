ec4f778bc0a339e34207b174925cacf93652d418
Gaurav Srivastava <gaurav001.s@samsung.com>
2015-11-13 11:15:18 -0800

    Several modifications to clean-up / enhance the sample:
    1. Fixed an initialization issue with GVRCylinderScneObject.
    2. Removed the code for rotating scene objects as it seems superfluous
    to what this sample intends to demonstrate.
    3. Attached the GVRCameraSceneObject to camera rig instead of main scene
    so that it can move with user's head motion.
    4. Enabled VRMode for the camera so that 60 fps camera preview can be used.

--- GVRf/Sample/scene-objects/src/org/gearvrf/sample/sceneobjects/SceneObjectActivity.java
+++ GVRf/Sample/scene-objects/src/org/gearvrf/sample/sceneobjects/SceneObjectActivity.java
@@ -17,9 +17,12 @@ package org.gearvrf.sample.sceneobjects;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
+import android.hardware.Camera.PreviewCallback;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -32,12 +35,6 @@ import org.gearvrf.scene_objects.view.GVRWebView;
 public class SceneObjectActivity extends GVRActivity {
     private static final String TAG = "SceneObjectActivity";
     private SampleViewManager mViewManager;
-    private float lastY = 0;
-    private float lastX = 0;
-    private float lastYAngle = 0;
-    private float lastXAngle = 0;
-    private float yangle = 0;
-    private float xangle = 0;
     private long lastDownTime = 0;
     private GVRWebView webView;
     private Camera camera;
@@ -75,7 +72,35 @@ public class SceneObjectActivity extends GVRActivity {
         return context.getPackageManager().hasSystemFeature(
                 PackageManager.FEATURE_CAMERA);
     }
+    
+    private long prevTime = 0;
+    private int numFrames = 0;
+    private PreviewCallback previewCallback = new PreviewCallback() {
 
+        @Override
+        /**
+         * The byte data comes from the android camera in the yuv format. so we
+         * need to convert it to rgba format.
+         */
+		public void onPreviewFrame(byte[] data, Camera camera) {
+			numFrames++;
+			if (numFrames == 100) {
+				Parameters params = SceneObjectActivity.this.camera
+						.getParameters();
+				params.setAutoWhiteBalanceLock(true);
+				params.setAutoExposureLock(true);
+				SceneObjectActivity.this.camera.setParameters(params);
+			}
+			long currentTime = System.currentTimeMillis();
+			Log.d(TAG,
+					"Preview Frame rate "
+							+ Math.round(1000 / (currentTime - prevTime)));
+			prevTime = currentTime;
+			camera.addCallbackBuffer(previewCallbackBuffer);
+		}
+	};
+
+    private byte[] previewCallbackBuffer = null;
     private void createCameraView() {
 
         if (!checkCameraHardware(this)) {
@@ -89,13 +114,47 @@ public class SceneObjectActivity extends GVRActivity {
             camera = Camera.open();
             if (camera != null) {
                 Parameters params = camera.getParameters();
-                params.setFocusMode(Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
-                camera.setParameters(params);
-                camera.startPreview();
-            }
-        } catch (Exception exception) {
-            android.util.Log.d(TAG, "Camera not available or is in use");
-        }
+                
+				// check if the device supports vr mode preview
+				if ("true".equalsIgnoreCase(params.get("vrmode-supported"))) {
+					Log.v(TAG, "VR Mode supported!");
+
+					// set vr mode
+					params.set("vrmode", 1);
+					
+					// true if the apps intend to record videos using MediaRecorder
+				    params.setRecordingHint(true); 
+
+					// set preview size
+					//params.setPreviewSize(640, 480);
+
+					// set fast-fps-mode: 0 for 30fps, 1 for 60 fps, 
+                    // 2 for 120 fps
+					params.set("fast-fps-mode", 1);
+					params.setPreviewFpsRange(60000, 60000);
+
+					// for auto focus
+					params.set("focus-mode", "continuous-video");
+
+					params.setVideoStabilization(false);
+					if ("true".equalsIgnoreCase(params.get("ois-supported"))) {
+						params.set("ois", "center");
+					}
+				}
+
+				camera.setParameters(params);
+				int bufferSize = params.getPreviewSize().height
+						* params.getPreviewSize().width
+						* ImageFormat
+								.getBitsPerPixel(params.getPreviewFormat()) / 8;
+				previewCallbackBuffer = new byte[bufferSize];
+				camera.addCallbackBuffer(previewCallbackBuffer);
+				camera.setPreviewCallbackWithBuffer(previewCallback);
+				camera.startPreview();
+			}
+		} catch (Exception exception) {
+			android.util.Log.d(TAG, "Camera not available or is in use");
+		}
     }
 
     GVRView getWebView() {
@@ -111,6 +170,7 @@ public class SceneObjectActivity extends GVRActivity {
         super.onPause();
         mViewManager.onPause();
         if (camera != null) {
+        	camera.setPreviewCallback(null);
             camera.stopPreview();
             camera.release();
             camera = null;
@@ -120,43 +180,14 @@ public class SceneObjectActivity extends GVRActivity {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
-            lastY = event.getY();
-            lastX = event.getX();
             lastDownTime = event.getDownTime();
         }
 
         if (event.getActionMasked() == MotionEvent.ACTION_UP) {
             // check if it was a quick tap
             if (event.getEventTime() - lastDownTime < 200) {
-                // if things are rotating, stop the rotation.
-                if (xangle != 0 || yangle != 0) {
-                    xangle = 0.0f;
-                    yangle = 0.0f;
-                    mViewManager.setXAngle(0.0f);
-                    mViewManager.setYAngle(0.0f);
-                    return true;
-                }
-
-                // otherwise, pass it as a tap to the ViewManager
-                mViewManager.setXAngle(0.0f);
-                mViewManager.setYAngle(0.0f);
+                // pass it as a tap to the ViewManager
                 mViewManager.onTap();
-            }
-
-            float xdifference = lastX - event.getX();
-            if (Math.abs(xdifference) > 10) {
-                xangle = lastXAngle + xdifference / 10;
-                mViewManager.setXAngle(1.0f);
-                lastX = event.getX();
-                lastXAngle = xangle;
-            }
-
-            float ydifference = lastY - event.getY();
-            if (Math.abs(ydifference) > 10) {
-                yangle = lastYAngle + ydifference / 10;
-                mViewManager.setYAngle(1.0f);
-                lastY = event.getY();
-                lastYAngle = yangle;
             }
         }
 
