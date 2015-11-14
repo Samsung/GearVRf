fa3e6ff6ef899c8b8a0daf45120097fac465935c
Danke Xie <danke.xie@gmail.com>
2015-11-14 10:59:57 -0800

    Add full Jassimp from assimp as a second importer
    Full Jassimp from assimp provide access to more information in loaded
    models than the current partial jassimp. That includes nodes, meshes, materials,
    textures, lights, cameras, bones, animations and more optimization options.
    
    Added a new conversion path from Assimp to GVRSceneObject: assimp->jassimp.cpp->
    Jassimp.java->GVRJassimpSceneObject. It allows users to access complete information
    from assimp in Java, and improves performance. The importer is invoked only once to
    get AiScene, and is not invoked during its traversal -- another big performance leap
    (N^2 --> N --> 1).
    
    The Jassimp is fetched from official assimp git:
    https://github.com/assimp/assimp/commit/57d7085fb8472a03048134eb56454a9c30943c34
    
    Major changes are:
    - Add Jassimp into org.gearvrf.jassimp2
    - Refactor AiWrapperProvider in GVRContext into org.gearvrf.jassimp
    - Add GVRContext.getAssimpModelNew to use full Jassimp to load a complete
      Assimp scene
    - Add GVRJassimpSceneObject to convert AiScene to GVRSceneObject
    - Fixed DeleteLocalRef issues in official jassimp.cpp

---
+++ GVRf/Framework/src/org/gearvrf/jassimp2/AiConfig.java
@@ -0,0 +1,15 @@
+/*
+ * $Revision$
+ * $Date$
+ */
+package org.gearvrf.jassimp2;
+
+
+/**
+ * Configuration interface for assimp importer.<p>
+ * 
+ * This class is work-in-progress
+ */
+public class AiConfig {
+
+}
