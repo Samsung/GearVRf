07a029379d2a1d60f8795fb962453a44c14bae79
Tom Flynn <tom.flynn@samsung.com>
2015-11-30 12:21:28 -0800

    removed intent handling code.
    removed intent handling code.  going back to making this a really
    minimal sample.

--- GVRf/Sample/gvrf_minimal360photo/src/org/gearvrf/samples/Minimal360Photo/Minimal360PhotoScript.java
+++ GVRf/Sample/gvrf_minimal360photo/src/org/gearvrf/samples/Minimal360Photo/Minimal360PhotoScript.java
@@ -15,60 +15,33 @@
 
 package org.gearvrf.samples.Minimal360Photo;
 
-import java.io.IOException;
 import java.util.concurrent.Future;
 
-import org.gearvrf.GVRActivity;
 import org.gearvrf.GVRAndroidResource;
-import org.gearvrf.GVRBitmapTexture;
 import org.gearvrf.GVRContext;
 import org.gearvrf.GVRScene;
 import org.gearvrf.GVRScript;
 import org.gearvrf.GVRTexture;
 import org.gearvrf.scene_objects.GVRSphereSceneObject;
 
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.provider.MediaStore;
-
 public class Minimal360PhotoScript extends GVRScript {
-
-    private final String TAG = "Minimal360PhotoScript";
-    private Bitmap mIncomingBitmap = null;
 
     @Override
     public void onInit(GVRContext gvrContext) {
 
-        android.util.Log.d(TAG, "in onInit");
         // get a handle to the scene
         GVRScene scene = gvrContext.getNextMainScene();
 
         GVRSphereSceneObject sphereObject = null;
 
-        if(mIncomingBitmap == null) {
-            // load texture
-            Future<GVRTexture> texture = gvrContext.loadFutureTexture(new GVRAndroidResource(gvrContext, R.raw.photosphere));
+        // load texture
+        Future<GVRTexture> texture = gvrContext.loadFutureTexture(new GVRAndroidResource(gvrContext, R.raw.photosphere));
 
-            // create a sphere scene object with the specified texture and triangles facing inward (the 'false' argument) 
-            sphereObject = new GVRSphereSceneObject(gvrContext, false, texture);
-        } else {
-            // create a sphere scene object with the triangles facing inward (the 'false' argument) 
-            sphereObject = new GVRSphereSceneObject(gvrContext, false);
-            // set the texture to be the specified incoming texture
-            GVRBitmapTexture incomingBitmapTexture = new GVRBitmapTexture(gvrContext, mIncomingBitmap);
-            sphereObject.getRenderData().getMaterial().setMainTexture(incomingBitmapTexture);
-        }
+        // create a sphere scene object with the specified texture and triangles facing inward (the 'false' argument) 
+        sphereObject = new GVRSphereSceneObject(gvrContext, false, texture);
 
         // add the scene object to the scene graph
         scene.addSceneObject(sphereObject);
-    }
-
-    public void setPhotosphere(GVRActivity activity, Uri uri) {
-        try {
-            mIncomingBitmap = MediaStore.Images.Media.getBitmap(activity.getContentResolver(), uri);
-        } catch (IOException e) {
-            android.util.Log.e(TAG, "Retrieving the bitmap failed.");
-        }
     }
 
     @Override
