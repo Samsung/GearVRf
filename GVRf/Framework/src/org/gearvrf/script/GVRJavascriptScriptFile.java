54cd2eff592e8f13de9be2c7ef23208661d6a3ef
Danke Xie <d.xie@samsung.com>
2016-01-15 18:04:23 -0800

    Add javadoc to GVRJavascriptScriptFile

--- GVRf/Framework/src/org/gearvrf/script/GVRJavascriptScriptFile.java
+++ GVRf/Framework/src/org/gearvrf/script/GVRJavascriptScriptFile.java
@@ -5,7 +5,36 @@ import java.io.InputStream;
 
 import org.gearvrf.GVRContext;
 
+/**
+ * Represents a Javascript file. The script text can be loaded in one
+ * of the following ways.
+ * <ul>
+ * <li>
+ *   Loaded from a {@link GVRAndroidResource} using {@link GVRScriptManager#loadScript(org.gearvrf.GVRAndroidResource, String)}.
+ * </li>
+ * <li>
+ *   Constructed locally and then set the text using {@link #setScriptText(String)}.
+ * </li>
+ * <li>
+ *   Constructed locally and then load the text using {@link #load(InputStream)}.
+ * </li>
+ * </ul>
+ *
+ * Once a script text is set or loaded, you can invoke functions in the
+ * script using {@link GVRScriptFile#invokeFunction(String, Object[]),
+ * or attach it to a scriptable object using {@link GVRScriptManager#attachScriptFile(IScriptable, GVRScriptFile)}
+ * to handle events delivered to it.
+ */
 public class GVRJavascriptScriptFile extends GVRScriptFile {
+    /**
+     * Loads a Javascript file from {@code inputStream}.
+     *
+     * @param gvrContext
+     *     The GVR Context.
+     * @param inputStream
+     *     The input stream from which the script is loaded.
+     * @throws IOException
+     */
     public GVRJavascriptScriptFile(GVRContext gvrContext, InputStream inputStream) throws IOException {
         super(gvrContext, GVRScriptManager.LANG_JAVASCRIPT);
         load(inputStream);
