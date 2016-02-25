b04ca14bd97dff0d77f6e86476e3ccc7f54a9144
Danke Xie <d.xie@samsung.com>
2016-02-25 11:58:40 -0800

    Defer GL for GVRCompressedTexture

--- GVRf/Framework/jni/objects/textures/compressed_texture.h
+++ GVRf/Framework/jni/objects/textures/compressed_texture.h
@@ -22,6 +22,7 @@
 
 //#include <GLES3/gl3.h>
 #include "objects/textures/texture.h"
+#include "util/gvr_jni.h"
 #include "util/gvr_log.h"
 
 namespace gvr {
@@ -31,21 +32,68 @@ public:
     // The constructor to use when loading a mipmap chain, from Java
     explicit CompressedTexture(GLenum target) :
             Texture(new GLTexture(target)), target(target) {
-        glBindTexture(target, gl_texture_->id());
+        pending_gl_task_ = GL_TASK_INIT_PLAIN;
     }
 
     // The constructor to use when loading a single-level texture
-    explicit CompressedTexture(GLenum target, GLenum internalFormat,
-            GLsizei width, GLsizei height, GLsizei imageSize, const char* data,
+    explicit CompressedTexture(JNIEnv* env, GLenum target, GLenum internalFormat,
+            GLsizei width, GLsizei height, GLsizei imageSize, jbyteArray bytes,
             int dataOffset, int* texture_parameters) :
             Texture(new GLTexture(target, texture_parameters)), target(target) {
-        glBindTexture(target, gl_texture_->id());
-        glCompressedTexImage2D(target, 0, internalFormat, width, height, 0,
-                imageSize, data + dataOffset);
+        pending_gl_task_ = GL_TASK_INIT_INTERNAL_FORMAT;
+
+        env_ = env;
+        internalFormat_ = internalFormat;
+        width_ = width;
+        height_ = height;
+        imageSize_ = imageSize;
+        bytesRef_ = static_cast<jbyteArray>(env->NewGlobalRef(bytes));
+        dataOffset_ = dataOffset;
+    }
+
+    virtual ~CompressedTexture() {
+        // Release global refs. Race condition does not occur because if
+        // the runPendingGL is running, the object won't be destructed.
+        switch (pending_gl_task_) {
+        case GL_TASK_INIT_INTERNAL_FORMAT:
+            env_->DeleteGlobalRef(bytesRef_);
+            break;
+
+        default:
+            break;
+        }
     }
 
     GLenum getTarget() const {
         return target;
+    }
+
+    virtual void runPendingGL() {
+        Texture::runPendingGL();
+
+        switch (pending_gl_task_) {
+        case GL_TASK_NONE:
+            return;
+
+        case GL_TASK_INIT_PLAIN:
+            glBindTexture(target, gl_texture_->id());
+            break;
+
+        case GL_TASK_INIT_INTERNAL_FORMAT: {
+            jbyte* data = env_->GetByteArrayElements(bytesRef_, 0);
+
+            glBindTexture(target, gl_texture_->id());
+            glCompressedTexImage2D(target, 0, internalFormat_, width_, height_, 0,
+                    imageSize_, data + dataOffset_);
+
+            env_->ReleaseByteArrayElements(bytesRef_, data, 0);
+            env_->DeleteGlobalRef(bytesRef_);
+            break;
+        }
+
+        } // switch
+
+        pending_gl_task_ = GL_TASK_NONE;
     }
 
 private:
@@ -56,6 +104,25 @@ private:
 
 private:
     GLenum const target;
+
+    // Enum for pending GL tasks. Keep a comma with each line
+    // for easier merging.
+    enum {
+        GL_TASK_NONE = 0,
+        GL_TASK_INIT_PLAIN,
+        GL_TASK_INIT_INTERNAL_FORMAT,
+    };
+    int pending_gl_task_;
+
+    JNIEnv* env_;
+
+    // For GL_TASK_INIT_INTERNAL_FORMAT
+    GLenum internalFormat_;
+    GLsizei width_;
+    GLsizei height_;
+    GLsizei imageSize_;
+    int dataOffset_;
+    jbyteArray bytesRef_;
 };
 
 }
