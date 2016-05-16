7501f8a5154d17c0a74b00d1386b543d8c243b6e
rahul <r.rudradevan@samsung.com>
2016-05-16 16:10:55 -0700

    Changes for 3D Cursor
    * Fixed set position for the mouse
    * Allow multiple gamepads to be used at once
    * Gaze now allows depth scrolling
    * Fixed keying in the input manager

--- GVRf/Framework/framework/src/main/java/org/gearvrf/io/GVRGazeCursorController.java
+++ GVRf/Framework/framework/src/main/java/org/gearvrf/io/GVRGazeCursorController.java
@@ -15,32 +15,43 @@
 
 package org.gearvrf.io;
 
-import org.gearvrf.GVRContext;
-import org.gearvrf.GVRDrawFrameListener;
-import org.joml.Vector3f;
-
-import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 
-class GVRGazeCursorController extends GVRBaseController
-        implements GVRDrawFrameListener {
+import org.gearvrf.GVRContext;
+import org.gearvrf.GVRDrawFrameListener;
+import org.gearvrf.utility.Log;
+import org.joml.Vector3f;
+
+class GVRGazeCursorController extends GVRBaseController implements
+        GVRDrawFrameListener {
     private static final String TAG = GVRGazeCursorController.class
             .getSimpleName();
-    private final KeyEvent BUTTON_GAZE_DOWN = new KeyEvent(KeyEvent.ACTION_DOWN,
-            KeyEvent.KEYCODE_BUTTON_1);
+    private static final int SET_KEY_DOWN = 1;
+    private static final int TAP_TIMEOUT = 160;
+    private static float TOUCH_SQUARE = 8.0f * 8.0f;
+    private static final float DEPTH_SENSITIVITY = 0.1f;
+    private final KeyEvent BUTTON_GAZE_DOWN = new KeyEvent(
+            KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_BUTTON_1);
     private final KeyEvent BUTTON_GAZE_UP = new KeyEvent(KeyEvent.ACTION_UP,
             KeyEvent.KEYCODE_BUTTON_1);
-
     private final GVRContext context;
     private int referenceCount = 0;
+    private boolean keyEventSent = false;
+    private float actionDownX;
+    private float actionDownY;
+    private float actionDownZ;
 
     // Used to calculate the absolute position that the controller reports to
     // the user.
     private final Vector3f gazePosition;
+    private Object lock = new Object();
 
     // Saves the relative position of the cursor with respect to the camera.
     private final Vector3f setPosition;
+    private GestureHandler handler;
 
     public GVRGazeCursorController(GVRContext context,
                                    GVRControllerType controllerType, String name, int vendorId,
@@ -49,6 +60,22 @@ class GVRGazeCursorController extends GVRBaseController
         this.context = context;
         gazePosition = new Vector3f();
         setPosition = new Vector3f();
+        handler = new GestureHandler();
+    }
+
+    // Use a handler like the one used in the gesture detector.
+    private class GestureHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SET_KEY_DOWN:
+                    keyEventSent = true;
+                    setKeyEvent(BUTTON_GAZE_DOWN);
+                    break;
+                default:
+                    Log.e(TAG, "Unknown message type");
+            }
+        }
     }
 
     /*
@@ -83,12 +110,42 @@ class GVRGazeCursorController extends GVRBaseController
     @Override
     boolean dispatchMotionEvent(MotionEvent event) {
         MotionEvent clone = MotionEvent.obtain(event);
-        if (clone.getAction() == MotionEvent.ACTION_DOWN) {
+        float eventX = event.getX();
+        float eventY = event.getY();
+        int action = clone.getAction();
+        if (action == MotionEvent.ACTION_DOWN) {
+            actionDownX = eventX;
+            actionDownY = eventY;
+            actionDownZ = setPosition.z;
             // report ACTION_DOWN as a button
-            setKeyEvent(BUTTON_GAZE_DOWN);
-        } else if (clone.getAction() == MotionEvent.ACTION_UP) {
+            handler.sendEmptyMessageAtTime(SET_KEY_DOWN, event.getDownTime()
+                    + TAP_TIMEOUT);
+        } else if (action == MotionEvent.ACTION_UP) {
             // report ACTION_UP as a button
-            setKeyEvent(BUTTON_GAZE_UP);
+            handler.removeMessages(SET_KEY_DOWN);
+            if (keyEventSent) {
+                setKeyEvent(BUTTON_GAZE_UP);
+                keyEventSent = false;
+            }
+        } else if (action == MotionEvent.ACTION_MOVE) {
+            float deltaX = eventX - actionDownX;
+            float deltaY = eventY - actionDownY;
+            float eventZ = actionDownZ + (deltaX * DEPTH_SENSITIVITY);
+
+            if (eventZ >= getNearDepth()) {
+                eventZ = getNearDepth();
+            }
+            if (eventZ <= getFarDepth()) {
+                eventZ = getFarDepth();
+            }
+
+            synchronized (lock) {
+                setPosition.z = eventZ;
+            }
+            float distance = (deltaX * deltaX) + (deltaY * deltaY);
+            if (distance > TOUCH_SQUARE) {
+                handler.removeMessages(SET_KEY_DOWN);
+            }
         }
         setMotionEvent(clone);
         return true;
@@ -102,8 +159,10 @@ class GVRGazeCursorController extends GVRBaseController
 
     @Override
     public void onDrawFrame(float frameTime) {
-        setPosition.mulPoint(context.getMainScene().getMainCameraRig()
-                .getHeadTransform().getModelMatrix4f(), gazePosition);
+        synchronized (lock) {
+            setPosition.mulPoint(context.getMainScene().getMainCameraRig()
+                    .getHeadTransform().getModelMatrix4f(), gazePosition);
+        }
         super.setPosition(gazePosition.x, gazePosition.y, gazePosition.z);
     }
 
