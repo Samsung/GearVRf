b4f6b4a90c64f729b09a8ed9c501c441bf9e840f
rahul <r.rudradevan@samsung.com>
2016-05-24 09:35:12 -0700

    ZipLoader: added a copyright header and fixed variable names

--- GVRf/Framework/framework/src/main/java/org/gearvrf/ZipLoader.java
+++ GVRf/Framework/framework/src/main/java/org/gearvrf/ZipLoader.java
@@ -1,3 +1,18 @@
+/* Copyright 2016 Samsung Electronics Co., LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.gearvrf;
 
 import android.content.Context;
@@ -64,7 +79,7 @@ public abstract class ZipLoader {
         Context context = gvrContext.getContext();
         InputStream inputStream = context.getAssets().open(zipFileName);
         ZipInputStream zipInputStream = new ZipInputStream(inputStream);
-        List<T> listTextures = new ArrayList<T>();
+        List<T> result = new ArrayList<T>();
 
         try {
             ZipEntry zipEntry;
@@ -83,12 +98,12 @@ public abstract class ZipLoader {
 
                 GVRAndroidResource androidResource = new GVRAndroidResource(zipEntry.getName(),
                         resourceInputStream);
-                T textureFuture = processor.getItem(gvrContext, androidResource);
-                listTextures.add(textureFuture);
+                T item = processor.getItem(gvrContext, androidResource);
+                result.add(item);
             }
         } finally {
             zipInputStream.close();
         }
-        return listTextures;
+        return result;
     }
 }
