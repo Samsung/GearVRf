c4190d8b6184d926baf33b61346474d1694c1bf7
Nola Donato <nola.donato@samsung.com>
2016-07-08 13:31:43 -0700

    Improved == checking more

--- GVRf/Framework/framework/src/main/java/org/gearvrf/jassimp2/GVRJassimpSceneObject.java
+++ GVRf/Framework/framework/src/main/java/org/gearvrf/jassimp2/GVRJassimpSceneObject.java
@@ -206,7 +206,7 @@ public class GVRJassimpSceneObject extends GVRModelSceneObject {
             if(texType != AiTextureType.UNKNOWN)
             {
                 final String texFileName = material.getTextureFile(texType, 0);
-                if ((texFileName != null) && !texFileName.equals(""))
+                if (!"".equals(texFileName))
                 {
                     GVRAssetLoader.TextureRequest texRequest = new GVRAssetLoader.MaterialTextureRequest(assetRequest.getContext(), texFileName, meshMaterial, textureMap.get(texType));
                     assetRequest.loadTexture(texRequest);
