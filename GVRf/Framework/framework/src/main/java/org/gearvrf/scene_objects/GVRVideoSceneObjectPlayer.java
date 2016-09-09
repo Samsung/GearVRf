ce15f9daf34e88a33c1c5adc11a2cc0af1d045be
Mihail Marinov <m.marinov@samsung.com>
2016-09-09 10:34:51 -0700

    GVRVideoSceneObject: changes to support video players other than the Android MediaPlayer
    GearVRf-DCO-1.0-Signed-off-by: Mihail Marinov <m.marinov@samsung.com>

---
+++ GVRf/Framework/framework/src/main/java/org/gearvrf/scene_objects/GVRVideoSceneObjectPlayer.java
@@ -0,0 +1,37 @@
+package org.gearvrf.scene_objects;
+
+import android.view.Surface;
+
+/**
+ * Use to supply different media player objects to GVRVideoSceneObject.
+ * For example in addition to the default MediaPlayer this can be used to make GVRVideoSceneObject
+ * utilize the ExoPlayer.
+ * @param <T>
+ */
+public interface GVRVideoSceneObjectPlayer<T> {
+    /**
+     * Returns the actual player instance
+     * @return
+     */
+    T getPlayer();
+
+    /**
+     * Called by GVRVideoSceneObject when it has the surface that can be passed to the media player
+     * @param surface
+     */
+    void setSurface(Surface surface);
+
+    /**
+     * Called by GVRVideoSceneObject when it is done with the media player; the media player and
+     * associated resources can be released.
+     */
+    void release();
+
+    /**
+     * Called by GVRVideoSceneObject to determine whether the Surface it creates internally can
+     * be released immediately after being passed to the media player. If this returns false it
+     * means that the implementation is responsible for releasing the Surface.
+     * @return
+     */
+    boolean canReleaseSurfaceImmediately();
+}
