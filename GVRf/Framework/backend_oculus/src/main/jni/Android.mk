389b0baf40dfbd9c2bb67131a6e7aefa2b871c73
rahul <r.rudradevan@samsung.com>
2017-04-13 11:48:07 -0700

    Update the framework build tools to 2.3.1

--- GVRf/Framework/backend_oculus/src/main/jni/Android.mk
+++ GVRf/Framework/backend_oculus/src/main/jni/Android.mk
@@ -61,7 +61,8 @@ LOCAL_CFLAGS := -Wattributes
 #LOCAL_LDLIBS += -ljnigraphics -lm_hard
 #softFP
 LOCAL_LDLIBS += -ljnigraphics -llog -lGLESv3 -lEGL -lz -landroid
-LOCAL_LDLIBS += $(PROJECT_DIR)/build/intermediates/exploded-aar/Framework/framework/unspecified/jni/$(TARGET_ARCH_ABI)/libgvrf.so
+LOCAL_LDLIBS += $(PROJECT_DIR)/../framework/build/intermediates/ndkBuild/$(APP_OPTIM)/obj/local/$(TARGET_ARCH_ABI)/libgvrf.so
+
 
 include $(BUILD_SHARED_LIBRARY)
 
