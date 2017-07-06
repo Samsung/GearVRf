ef18294915e3b4dc4b53f518cbb91e03c109e8bc
Dmitriy Vasilev <d.vasilev@samsung.com>
2017-07-06 09:15:34 -0700

    vulkan: Fix some Java-side leaks (#1064), amended
    This big commit was not completely ported to the vulkan branch.
    The remaining part contains:
    - mDefaultTextureParameters rename
    - New file GVRContextProxy.java
    - GVRScriptManager.destroy()

---
+++ GVRf/Framework/framework/src/main/java/org/gearvrf/script/GVRContextProxy.java
@@ -0,0 +1,208 @@
+package org.gearvrf.script;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import org.gearvrf.GVRActivity;
+import org.gearvrf.GVRAndroidResource;
+import org.gearvrf.GVRAssetLoader;
+import org.gearvrf.GVRAtlasInformation;
+import org.gearvrf.GVRBitmapTexture;
+import org.gearvrf.GVRContext;
+import org.gearvrf.GVRCubemapTexture;
+import org.gearvrf.GVRDrawFrameListener;
+import org.gearvrf.GVREventManager;
+import org.gearvrf.GVREventReceiver;
+import org.gearvrf.GVRImportSettings;
+import org.gearvrf.GVRMaterialShaderManager;
+import org.gearvrf.GVRMesh;
+import org.gearvrf.GVRPostEffectShaderManager;
+import org.gearvrf.GVRScene;
+import org.gearvrf.GVRSceneObject;
+import org.gearvrf.GVRScreenshot3DCallback;
+import org.gearvrf.GVRScreenshotCallback;
+import org.gearvrf.GVRTexture;
+import org.gearvrf.GVRTextureParameters;
+import org.gearvrf.animation.GVRAnimationEngine;
+import org.gearvrf.debug.DebugServer;
+import org.gearvrf.io.GVRInputManager;
+import org.gearvrf.periodic.GVRPeriodicEngine;
+import org.gearvrf.scene_objects.GVRModelSceneObject;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.concurrent.Future;
+
+public final class GVRContextProxy {
+    private final WeakReference<GVRContext> mContext;
+
+    GVRContextProxy(GVRContext context) {
+        mContext = new WeakReference<GVRContext>(context);
+    }
+
+    public Context getContext() {
+        return mContext.get().getContext();
+    }
+
+    public GVRActivity getActivity() {
+        return mContext.get().getActivity();
+    }
+
+    public GVRAssetLoader getAssetLoader() {
+        return mContext.get().getAssetLoader();
+    }
+
+    public GVREventReceiver getEventReceiver() {
+        return mContext.get().getEventReceiver();
+    }
+
+    public GVRMesh loadMesh(GVRAndroidResource androidResource) {
+        return mContext.get().loadMesh(androidResource);
+    }
+
+    public GVRMesh loadMesh(GVRAndroidResource androidResource, EnumSet<GVRImportSettings> settings) {
+        return mContext.get().loadMesh(androidResource, settings);
+    }
+
+    public void loadMesh(GVRAndroidResource.MeshCallback callback, GVRAndroidResource androidResource) throws IllegalArgumentException {
+        mContext.get().loadMesh(callback, androidResource);
+    }
+
+    public void loadMesh(GVRAndroidResource.MeshCallback callback, GVRAndroidResource resource, int priority) throws IllegalArgumentException {
+        mContext.get().loadMesh(callback, resource, priority);
+    }
+
+    public Future<GVRMesh> loadFutureMesh(GVRAndroidResource resource) {
+        return mContext.get().loadFutureMesh(resource);
+    }
+
+    public Future<GVRMesh> loadFutureMesh(GVRAndroidResource resource, int priority) {
+        return mContext.get().loadFutureMesh(resource, priority);
+    }
+
+    public GVRMesh createQuad(float width, float height) {
+        return mContext.get().createQuad(width, height);
+    }
+
+    public void assertGLThread() {
+        mContext.get().assertGLThread();
+    }
+
+    public boolean isCurrentThreadGLThread() {
+        return mContext.get().isCurrentThreadGLThread();
+    }
+
+    public List<GVRAtlasInformation> loadTextureAtlasInformation(GVRAndroidResource resource) throws IOException {
+        return mContext.get().loadTextureAtlasInformation(resource);
+    }
+
+    public GVRScene getMainScene() {
+        return mContext.get().getMainScene();
+    }
+
+    public void setMainScene(GVRScene scene) {
+        mContext.get().setMainScene(scene);
+    }
+
+    public DebugServer startDebugServer() {
+        return mContext.get().startDebugServer();
+    }
+
+    public synchronized DebugServer startDebugServer(int port, int maxClients) {
+        return mContext.get().startDebugServer(port, maxClients);
+    }
+
+    public void logError(String message, Object sender) {
+        mContext.get().logError(message, sender);
+    }
+
+    public synchronized void stopDebugServer() {
+        mContext.get().stopDebugServer();
+    }
+
+    public GVRInputManager getInputManager() {
+        return mContext.get().getInputManager();
+    }
+
+    public GVREventManager getEventManager() {
+        return mContext.get().getEventManager();
+    }
+
+    public GVRScriptManager getScriptManager() {
+        return mContext.get().getScriptManager();
+    }
+
+    public float getFrameTime() {
+        return mContext.get().getFrameTime();
+    }
+
+    public void runOnGlThread(Runnable runnable) {
+        mContext.get().runOnGlThread(runnable);
+    }
+
+    public void runOnGlThreadPostRender(int delayFrames, Runnable runnable) {
+        mContext.get().runOnGlThreadPostRender(delayFrames, runnable);
+    }
+
+    public void registerDrawFrameListener(GVRDrawFrameListener frameListener) {
+        mContext.get().registerDrawFrameListener(frameListener);
+    }
+
+    public void unregisterDrawFrameListener(GVRDrawFrameListener frameListener) {
+        mContext.get().unregisterDrawFrameListener(frameListener);
+    }
+
+    public GVRMaterialShaderManager getMaterialShaderManager() {
+        return mContext.get().getMaterialShaderManager();
+    }
+
+    public GVRPostEffectShaderManager getPostEffectShaderManager() {
+        return mContext.get().getPostEffectShaderManager();
+    }
+
+    public GVRAnimationEngine getAnimationEngine() {
+        return mContext.get().getAnimationEngine();
+    }
+
+    public GVRPeriodicEngine getPeriodicEngine() {
+        return mContext.get().getPeriodicEngine();
+    }
+
+    public void captureScreenCenter(GVRScreenshotCallback callback) {
+        mContext.get().captureScreenCenter(callback);
+    }
+
+    public void captureScreenLeft(GVRScreenshotCallback callback) {
+        mContext.get().captureScreenLeft(callback);
+    }
+
+    public void captureScreenRight(GVRScreenshotCallback callback) {
+        mContext.get().captureScreenRight(callback);
+    }
+
+    public void captureScreen3D(GVRScreenshot3DCallback callback) {
+        mContext.get().captureScreen3D(callback);
+    }
+
+    public void setTag(Object tag) {
+        mContext.get().setTag(tag);
+    }
+
+    public Object getTag() {
+        return mContext.get().getTag();
+    }
+
+    public void runOnTheFrameworkThread(Runnable runnable) {
+        mContext.get().runOnTheFrameworkThread(runnable);
+    }
+
+    public void showToast(String message) {
+        mContext.get().showToast(message);
+    }
+
+    public void showToast(String message, float duration) {
+        mContext.get().showToast(message, duration);
+    }
+}
