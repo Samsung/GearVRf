b2e4d9f5e9dd75b1c21bcb2bf3988f69159e07cf
Mihail Marinov <m.marinov@samsung.com>
2017-12-01 17:11:21 -0800

    framework: fix possible npe in the dockeventreceiver

--- GVRf/Framework/framework/src/main/java/org/gearvrf/utility/DockEventReceiver.java
+++ GVRf/Framework/framework/src/main/java/org/gearvrf/utility/DockEventReceiver.java
@@ -1,4 +1,17 @@
-
+/* Copyright 2016 Samsung Electronics Co., LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gearvrf.utility;
 
 import android.content.BroadcastReceiver;
@@ -37,9 +50,9 @@ public final class DockEventReceiver {
         @Override
         public void onReceive(Context context, final Intent intent) {
             Log.v(TAG, "received " + intent);
-            if (ACTION_HMT_DISCONNECT.equals(intent.getAction())) {
+            if (ACTION_HMT_DISCONNECT.equals(intent.getAction()) && null != mRunOnUndock) {
                 mRunOnUndock.run();
-            } else if (ACTION_HMT_CONNECT.equals(intent.getAction())) {
+            } else if (ACTION_HMT_CONNECT.equals(intent.getAction()) && null != mRunOnDock) {
                 mRunOnDock.run();
             }
         }
