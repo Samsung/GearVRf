a5684c12a99aa7a39fd983d6645dade3bab8b648
Mihail Marinov <m.marinov@samsung.com>
2018-04-04 08:25:42 -0700

    framework: add nullptr check

--- GVRf/Framework/framework/src/main/jni/objects/components/mesh_collider.cpp
+++ GVRf/Framework/framework/src/main/jni/objects/components/mesh_collider.cpp
@@ -71,6 +71,10 @@ namespace gvr
         Mesh* mesh = mesh_;
         bool pickCoordinates = pickCoordinates_;
         SceneObject* owner = owner_object();
+        if (nullptr == owner) {
+            return ColliderData();
+        }
+
         RenderData* rd = owner->render_data();
         glm::mat4 model_matrix = owner->transform()->getModelMatrix();
         glm::mat4 model_inverse = glm::affineInverse(model_matrix);
@@ -130,6 +134,10 @@ namespace gvr
     {
         Mesh* mesh = mesh_;
         SceneObject* owner = owner_object();
+        if (nullptr == owner) {
+            return ColliderData();
+        }
+
         RenderData* rd = owner->render_data();
         glm::mat4 model_matrix = owner->transform()->getModelMatrix();
         glm::mat4 model_inverse = glm::affineInverse(model_matrix);
