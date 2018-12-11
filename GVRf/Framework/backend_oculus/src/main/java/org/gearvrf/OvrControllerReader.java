8cfdc4ea50a6edd23c519faa207a3521cd9ec979
Mihail Marinov <m.marinov@samsung.com>
2018-12-11 09:35:38 -0800

    Oculus GO: avoid occasional double back key generation

--- GVRf/Framework/backend_oculus/src/main/java/org/gearvrf/OvrControllerReader.java
+++ GVRf/Framework/backend_oculus/src/main/java/org/gearvrf/OvrControllerReader.java
@@ -72,7 +72,7 @@ final class OvrControllerReader extends GVRGearCursorController.ControllerReader
 
         @Override
         public void dispatchKeyEvent(final KeyEvent event) {
-            if (KeyEvent.KEYCODE_BACK == event.getKeyCode()) {
+            if (KeyEvent.KEYCODE_BACK == event.getKeyCode() && KeyEvent.ACTION_DOWN == event.getAction()) {
                 mApplication.getActivity().dispatchKeyEvent(event);
             }
         }
